637dd6a0e968b91a0f71549c836db2beda513476
Mohammed159159 <mohammedhany300@gmail.com>
2023-01-08 01:09:54 +0200

    dev: Start Header container

--- frontend_next/pages/containers/Header/Header.tsx
+++ frontend_next/pages/containers/Header/Header.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
+import style from "./Header.module.scss"
+import {motion} from "framer-motion"
 
-function Header(props: unknown) {
-    return <h1>Header</h1>;
+function Header() {
+    return (
+        <div className={style["app__header app__flex"]}>
+            <motion.div
+                className={style["app__header-welcome"]}
+                animate={{ x: [-100, 0], opacity: [0, 1] }}
+                transition={{duration: 0.5}}
+            >
+                <div style={{marginLeft: 300, paddingTop: 300}}>
+                <span>👋</span>
+                <p className="p-text">Hello, I am Mohammed</p>
+                </div>
+            </motion.div>
+        </div>
+    );
 }
 
 export default Header;
