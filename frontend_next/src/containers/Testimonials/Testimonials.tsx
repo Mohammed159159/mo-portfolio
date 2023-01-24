f0131bda58851d23cdf5dc860a2511e9a63cdc28
Mohammed159159 <mohammedhany300@gmail.com>
2023-01-24 16:35:28 +0200

    dev: Finish Testimonials section

--- frontend_next/src/containers/Testimonials/Testimonials.tsx
+++ frontend_next/src/containers/Testimonials/Testimonials.tsx
@@ -1,7 +1,124 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import { easeIn, motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { client, urlFor } from "../../api/client";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import style from "./Testimonials.module.scss";
 
-function Testimonials(props: unknown) {
-    return <h1>Testimonials</h1>;
+type Testimonial = {
+    name: string;
+    company: string;
+    imgurl: string;
+    feedback: string;
+};
+
+type Brand = {
+    name: string;
+    imgUrl: string;
+};
+
+function Testimonials() {
+    //Get testimonials from sanity
+    const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
+    //watch active testimonial
+    const [currentIndex, setCurrentIndex] = useState<number>(0);
+    useEffect(() => {
+        return () => {
+            const query = '*[_type == "testimonials"]';
+            client.fetch(query).then((data) => setTestimonials(data));
+        };
+    }, []);
+    const currentTestimonial = testimonials[currentIndex];
+
+    const handleCardNav = (index: number): void => {
+        setCurrentIndex(index);
+    };
+
+    //Get brands from sanity
+    const [brands, setBrands] = useState<Brand[]>([]);
+    useEffect(() => {
+        return () => {
+            const query = '*[_type == "brands"]';
+            client.fetch(query).then((data) => setBrands(data));
+        };
+    }, []);
+
+    return (
+        <div className={style["app__testimonials"]}>
+            {testimonials["length"] && (
+                <>
+                    <div
+                        className={`${style["app__testimonial-card"]} app__flex`}
+                    >
+                        <img
+                            src={urlFor(currentTestimonial["imgurl"]).url()}
+                            alt={currentTestimonial["name"]}
+                        />
+                        <div className={`${style["app__testimonial-content"]}`}>
+                            <p className="p-text">
+                                {currentTestimonial["feedback"]}
+                            </p>
+                            <h4 className="bold-text">
+                                {currentTestimonial["name"]}
+                            </h4>
+                            <h5 className="p-text">
+                                {currentTestimonial["company"]}
+                            </h5>
+                        </div>
+                    </div>
+
+                    <div
+                        className={`${style["app__testimonial-nav"]} app__flex`}
+                    >
+                        <motion.div
+                            whileInView={{ x: [-30, 0] }}
+                            transition={{type: easeIn}}
+                            className="app__flex"
+                            onClick={() => {
+                                handleCardNav(
+                                    currentIndex === 0
+                                        ? testimonials.length - 1
+                                        : currentIndex - 1
+                                );
+                            }}
+                        >
+                            <HiChevronLeft />
+                        </motion.div>
+                        <motion.div
+                            whileInView={{ x: [30, 0] }}
+                            transition={{type: easeIn}}
+                            className="app__flex"
+                            onClick={() =>
+                                handleCardNav(
+                                    (currentIndex + 1) % testimonials.length
+                                )
+                            }
+                        >
+                            <HiChevronRight />
+                        </motion.div>
+                    </div>
+                </>
+            )}
+
+            <div className={`${style["app__testimonials-brands"]} app__flex`}>
+                {
+                    brands.map((brand, index) => 
+                        <motion.div key={`${index}-${brand}`}
+                            whileInView={{ opacity: [0, 1] }}
+                            transition={{duration: 0.5, type: "tween"}}
+                        >
+                            <img src={urlFor(brand["imgUrl"]).url()} alt={brand["name"]} />
+                    </motion.div>
+                    )
+                }
+            </div>
+        </div>
+    );
 }
 
-export default Testimonials;
+export default AppWrap(
+    MotionWrap(Testimonials, "app__testimonials"),
+    "testimonials",
+    "app__primarybg"
+);
