58d5322a4e9e0ad75da2d3b452ddd70ccc74d248
Mohammed159159 <mohammedhany300@gmail.com>
2023-01-25 20:37:17 +0200

    dev: Add env vars to client.ts

--- frontend_next/src/api/client.ts
+++ frontend_next/src/api/client.ts
@@ -2,11 +2,12 @@ import sanityClient from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url"
 import { SanityImageSource } from "@sanity/image-url/lib/types/types"
 export const client = sanityClient({
-    projectId: "fswwnj2d",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+    ignoreBrowserTokenWarning: true //not good :(
 
 })
  
