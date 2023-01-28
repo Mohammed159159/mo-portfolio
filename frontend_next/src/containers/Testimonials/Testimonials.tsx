29adf6689fd1fc0f6499b476616e625500ced0ea
Mohammed159159 <mohammedhany300@gmail.com>
2023-01-28 18:32:36 +0200

    fix: Repare testimonials not showing, finally

--- frontend_next/src/containers/Testimonials/Testimonials.tsx
+++ frontend_next/src/containers/Testimonials/Testimonials.tsx
@@ -18,17 +18,15 @@ type Brand = {
     imgUrl: string;
 };
 
-function Testimonials() {
+export function Testimonials() {
     //Get testimonials from sanity
     const [testimonials, setTestimonials] = useState<Testimonial[]>([]);
     //watch active testimonial
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     useEffect(() => {
-        return () => {
             const query = '*[_type == "testimonials"]';
             client.fetch(query).then((data) => setTestimonials(data));
-        };
-    }, []);
+    });
 
     const handleCardNav = (index: number): void => {
         setCurrentIndex(index);
@@ -41,63 +39,64 @@ function Testimonials() {
             const query = '*[_type == "brands"]';
             client.fetch(query).then((data) => setBrands(data));
         };
-    }, []);
+    });
 
-    if(testimonials.length == 0) return <h2 className="head-text">Loading...</h2>
     return (
         <div className={style["app__testimonials"]}>
+            {testimonials.length && (
                 <>
-                    <div
-                        className={`${style["app__testimonial-card"]} app__flex`}
-                    >
-                        <img
-                            src={urlFor(testimonials[currentIndex]["imgurl"]).url()}
-                            alt={testimonials[currentIndex]["name"]}
-                        />
-                        <div className={`${style["app__testimonial-content"]}`}>
-                            <p className="p-text">
-                                {testimonials[currentIndex]["feedback"]}
-                            </p>
-                            <h4 className="bold-text">
-                                {testimonials[currentIndex]["name"]}
-                            </h4>
-                            <h5 className="p-text">
-                                {testimonials[currentIndex]["company"]}
-                            </h5>
-                        </div>
+                <div
+                    className={`${style["app__testimonial-card"]} app__flex`}
+                >
+                    <img
+                        src={urlFor(testimonials[currentIndex]["imgurl"]).url()}
+                        alt={testimonials[currentIndex]["name"]}
+                    />
+                    <div className={`${style["app__testimonial-content"]}`}>
+                        <p className="p-text">
+                            {testimonials[currentIndex]["feedback"]}
+                        </p>
+                        <h4 className="bold-text">
+                            {testimonials[currentIndex]["name"]}
+                        </h4>
+                        <h5 className="p-text">
+                            {testimonials[currentIndex]["company"]}
+                        </h5>
                     </div>
+                </div>
 
-                    <div
-                        className={`${style["app__testimonial-nav"]} app__flex`}
+                <div
+                    className={`${style["app__testimonial-nav"]} app__flex`}
+                >
+                    <motion.div
+                        whileInView={{ x: [-30, 0] }}
+                        transition={{type: easeIn}}
+                        className="app__flex"
+                        onClick={() => {
+                            handleCardNav(
+                                currentIndex === 0
+                                    ? testimonials.length - 1
+                                    : currentIndex - 1
+                            );
+                        }}
                     >
-                        <motion.div
-                            whileInView={{ x: [-30, 0] }}
-                            transition={{type: easeIn}}
-                            className="app__flex"
-                            onClick={() => {
-                                handleCardNav(
-                                    currentIndex === 0
-                                        ? testimonials.length - 1
-                                        : currentIndex - 1
-                                );
-                            }}
-                        >
-                            <HiChevronLeft />
-                        </motion.div>
-                        <motion.div
-                            whileInView={{ x: [30, 0] }}
-                            transition={{type: easeIn}}
-                            className="app__flex"
-                            onClick={() =>
-                                handleCardNav(
-                                    (currentIndex + 1) % testimonials.length
-                                )
-                            }
-                        >
-                            <HiChevronRight />
-                        </motion.div>
-                    </div>
-                </>
+                        <HiChevronLeft />
+                    </motion.div>
+                    <motion.div
+                        whileInView={{ x: [30, 0] }}
+                        transition={{type: easeIn}}
+                        className="app__flex"
+                        onClick={() =>
+                            handleCardNav(
+                                (currentIndex + 1) % testimonials.length
+                            )
+                        }
+                    >
+                        <HiChevronRight />
+                    </motion.div>
+                </div>
+            </>
+            )}
 
             <div className={`${style["app__testimonials-brands"]} app__flex`}>
                 {
